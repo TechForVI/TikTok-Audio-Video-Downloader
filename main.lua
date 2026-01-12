834eccd79dc5f6c462b62b7f0abcd77f5161869e
TechForVI <sabirrana112211@gmail.com>
2026-01-12 21:32:37 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ import "java.io.File"
 
 activity = this
 
-local CURRENT_VERSION = "1.0"
+local CURRENT_VERSION = "1.1"
 local GITHUB_RAW_URL = "https://raw.githubusercontent.com/TechForVI/TikTok-Audio-Video-Downloader/main/"
 local VERSION_URL = GITHUB_RAW_URL .. "version.txt"
 local SCRIPT_URL = GITHUB_RAW_URL .. "main.lua"
@@ -53,18 +53,13 @@ function downloadAndInstallUpdate()
       if f then
         f:write(newContent)
         f:close()
-        -- پرانی فائل ہٹانے اور نئی فائل سیٹ کرنے کا عمل
         os.remove(PLUGIN_PATH)
-        if os.rename(tempPath, PLUGIN_PATH) then
-           service.speak("Update successful! Please restart the plugin.")
-           -- یہاں activity.finish کی ضرورت نہیں، صرف ڈائیلاگ بند کریں
-           if dlg then dlg.dismiss() end 
-        else
-           service.speak("Failed to replace the plugin file.")
-        end
+        os.rename(tempPath, PLUGIN_PATH)
+        service.speak("Update successful! Please restart the plugin.")
+        activity.finish()
       end
     else
-      service.speak("Update download failed.")
+      service.speak("Update failed.")
     end
   end)
 end
